e44d4b5d1a5b050e1948077c3b7b095cdeaa550e
Kenneth Bryan Chung <kbchung18@gmail.com>
2017-03-11 00:20:20 +0800

    start date filter show rooms

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -2,7 +2,7 @@ class RoomSerializer < ActiveModel::Serializer
   attributes *%i(name tag schedule_for_day)
 
   def schedule_for_day
-    puts "date param: #{scope[:params][:date]}"
+    puts "date param: #{scope[:params][:start_date]}"
     object.schedules.where('start_date BETWEEN ? AND ?', scope[:params][:start_date].beginning_of_day, scope[:params][:start_date].end_of_day)
   end
 end
